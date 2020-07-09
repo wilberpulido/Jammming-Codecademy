40c7de49182ae7626c1a22d4ce418971a350e2b7
unknown <pulidowilber@gmail.com>
2020-07-09 18:20:40 -0300

    Primer commit, paso 62

---
+++ src/Components/App/App.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import './App.css';
+
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist'
+
+//Tentremos que importar Playlist SearchBar   SearchResults
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state =({
+      searchResults: [
+        {
+          name: 'No te merezco',
+          artists: 'Micro TDH',
+          album: 'Lo que sea',
+          id: 1
+        },
+        {
+          name: 'Pensando en ti',
+          artists: 'Canserbero',
+          album: 'Vida',
+          id: 2
+        }
+      ],
+
+      playListName: 'Lacreo.com',
+
+      playlistTracks: [
+        {
+          name: 'Kafune',
+          artists: 'Micro TDH',
+          album: 'Lo que sea',
+          id: 3
+        },
+        {
+          name: 'Gordo fonky',
+          artists: 'Akapellah',
+          album: 'Que se yo',
+          id: 4
+        }
+      ]
+      //Array de objetos, cada uno tiene propiedades como name, artists, album y id.
+    });
+
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist= this.savePlaylist.bind(this);
+
+  }
+  addTrack(track){
+    const tracks = this.state.playlistTracks;
+    if (tracks.find(savedTrack => savedTrack.id === track.id)){
+      return;
+    }
+
+    tracks.push(track);
+    this.setState({playlistTracks: tracks});
+  }
+  removeTrack(track){
+    const tracks = this.state.playlistTracks;
+    if (tracks.find(tracks => tracks.id === track.id)){    
+      let aux = this.tracks.findIndex(tracks=> tracks.id === track.id);
+      
+      this.setState({
+        playlistTracks: this.state.playlistTracks.splice(aux,1)
+      })
+    }
+  }
+  updatePlaylistName(name){
+    this.setState({
+      playListName: name,
+    })
+  }
+  savePlaylist(){
+    alert('Este metodo ha sido usado')
+    const trackURIs = this.state.playlistTracks.map(track => track.uri);
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>Ja<span className = "highlight">mmm</span>ing</h1>
+        <div className = "App">
+            {/* <!-- Add a SearchBar component --> */}
+          <div className = "App-playlist">
+            <SearchResults searchResults={this.state.searchResults}
+              onAdd = {this.addTrack}/>
+            <Playlist playListName={this.state.playListName}
+                  playlistTracks={this.state.playlistTracks}
+                  onRemove = {this.removeTrack} 
+                  onNameChange = {this.updatePlaylistName}
+                  onSave ={this.savePlaylist}/>
+          </div>
+      </div>
+      </div>
+  );}
+}
+
+export default App;
